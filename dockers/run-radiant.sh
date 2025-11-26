56506cd2af1b8084595aefe48532c2e023e8c3bf
Justin Berger <j.david.berger@gmail.com>
2025-11-26 08:36:34 -0700

    Add utility scripts to run radiant images

---
+++ dockers/run-radiant.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+RADIANT_IMAGE=${RADIANT_IMAGE:-radiant2024.1}
+
+if [ ! -n "$(docker images -q "$RADIANT_IMAGE" 2> /dev/null)" ]; then
+    DOCKER_ENGINE build . -f Dockerfile.radiant --target $RADIANT_IMAGE -t $RADIANT_IMAGE
+fi
+
+RADIANT_HOSTNAME=$HOSTNAME
+for env_file in $HOME/.config/radiant.env $HOME/.config/radiant-$RADIANT_IMAGE.env $SCRIPT_DIR/../radiant.env $SCRIPT_DIR/../radiant-$RADIANT_IMAGE.env; do
+  if [ -f $env_file ]; then
+    source $env_file
+    FOUND_RADIANT_CONFIG=1
+    echo "Loading radiant env file from $env_file"
+  fi
+done
+
+if [[ ! $FOUND_RADIANT_CONFIG ]]; then
+  echo "No radiant config file found. One needs to exist that looks like:"
+  echo "export LICENSE_MAC=02:03:04:05:06:07"
+  echo "export DATA_PATH=/path/to/lscc"
+  exit -1
+fi
+
+if [[ -v LICENSE_MAC ]]; then
+  MAC_OPTION=--mac-address=$LICENSE_MAC
+fi
+
+docker run -it --rm \
+       $MAC_OPTION \
+       -l \
+       -v$HOME/.config/LatticeSemi:/home/user/.config/LatticeSemi \
+       -v$HOME/.config/RadiantIPLocal:/home/user/RadiantIPLocal \
+       -v$HOME:$HOME \
+       -v`pwd`:`pwd` \
+       -w `pwd` \
+       -v /tmp/.X11-unix:/tmp/.X11-unix \
+       -e DISPLAY=$DISPLAY        \
+       -h $RADIANT_HOSTNAME \
+       -v $HOME/.Xauthority:/home/user/.Xauthority \
+       -v $DATA_PATH:/data \
+       $RADIANT_IMAGE $@
