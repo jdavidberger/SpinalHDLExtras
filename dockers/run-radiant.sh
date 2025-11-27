054ff39c079c489dade710f86ab7e8c40fba935f
Justin Berger <j.david.berger@gmail.com>
2025-11-27 11:59:29 -0700

    Make work for podman

--- dockers/run-radiant.sh
+++ dockers/run-radiant.sh
@@ -2,19 +2,12 @@
 
 set -x
 
-DOCKER_ENGINE=${DOCKER_ENGINE:-docker}
+DOCKER_ENGINE=${DOCKER_ENGINE:-podman}
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-RADIANT_VERSION=${RADIANT_VERSION:-2024.2}
-RADIANT_IMAGE=${RADIANT_IMAGE:-radiant$RADIANT_VERSION}
-
-#if [ ! -n "$(docker images -q "$RADIANT_IMAGE" 2> /dev/null)" ]; then
-$DOCKER_ENGINE build --build-arg USER_UID=$(id -u ${USER}) --build-arg GROUP_GID=$(id -g ${USER}) $SCRIPT_DIR -f $SCRIPT_DIR/Dockerfile.radiant --target $RADIANT_IMAGE -t $RADIANT_IMAGE
-#fi
 
 RADIANT_HOSTNAME=$HOSTNAME
 RADIANT_BASE=/opt/lattice/radiant/
-for env_file in $HOME/.config/radiant.env $HOME/.config/radiant-$RADIANT_IMAGE.env $SCRIPT_DIR/../radiant.env $SCRIPT_DIR/../radiant-$RADIANT_IMAGE.env; do
+for env_file in $HOME/.config/radiant.env $SCRIPT_DIR/../radiant.env; do
   if [ -f $env_file ]; then
     source $env_file
     FOUND_RADIANT_CONFIG=1
@@ -22,13 +15,18 @@ for env_file in $HOME/.config/radiant.env $HOME/.config/radiant-$RADIANT_IMAGE.e
   fi
 done
 
-LM_LICENSE_FILE="$RADIANT_BASE/$RADIANT_VERSION/license/license.dat"
+RADIANT_VERSION=${RADIANT_VERSION:-2024.2}
+RADIANT_IMAGE=${RADIANT_IMAGE:-radiant$RADIANT_VERSION}
+
+$DOCKER_ENGINE build --build-arg USER_UID=$(id -u ${USER}) --build-arg GROUP_GID=$(id -g ${USER}) $SCRIPT_DIR -f $SCRIPT_DIR/Dockerfile.radiant --target $RADIANT_IMAGE -t $RADIANT_IMAGE
+
+LM_LICENSE_FILE=${LM_LICENSE_FILE-"$RADIANT_BASE/$RADIANT_VERSION/license/license.dat"}
 LICENSE_MAC=$(grep -m1 -oE 'HOSTID=[0-9a-fA-F]+' "$LM_LICENSE_FILE" | cut -d= -f2)
 LICENSE_MAC=$(echo "$LICENSE_MAC" | sed 's/../&:/g; s/:$//')
 
-MAC_OPTION=--mac-address=$LICENSE_MAC
+MAC_OPTION="--mac-address=$LICENSE_MAC --network bridge"
 
-$DOCKER_ENGINE run -it --rm \
+$DOCKER_ENGINE run --userns=keep-id -it --rm \
        $MAC_OPTION \
        -l \
        -v$HOME/.config/LatticeSemi:/home/user/.config/LatticeSemi \
