f6b2527cd4bfaed97108043810ae8dafa6fa2d77
Justin Berger <j.david.berger@gmail.com>
2025-12-09 11:53:15 -0700

    Use container image for ci

--- dockers/run-spinal-image.sh
+++ dockers/run-spinal-image.sh
@@ -3,7 +3,13 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 DOCKER_ENGINE=${DOCKER_ENGINE:-podman}
-${DOCKER_ENGINE} build --build-arg USER_UID=$(id -u ${USER}) $SCRIPT_DIR -f Dockerfile.spinalhdl -t spinal-image --progress plain
+IMAGE_NAME=ghcr.io/jdavidberger/spinalhdlextras/spinal-image:latest
+
+if [[ ! -z $PULL_IMAGE ]]; then
+  ${DOCKER_ENGINE} pull $IMAGE_NAME
+else
+  ${DOCKER_ENGINE} build --build-arg USER_UID=$(id -u ${USER}) $SCRIPT_DIR -f Dockerfile.spinalhdl -t $IMAGE_NAME --progress plain
+fi
 
 mkdir -p "$(pwd)/.${DOCKER_ENGINE}/.cache"
 mkdir -p "$(pwd)/.${DOCKER_ENGINE}/.sbt"
@@ -16,4 +22,4 @@ ${DOCKER_ENGINE} run --userns=keep-id \
   -v "$(pwd)/.${DOCKER_ENGINE}/.sbt:/home/user/.sbt" \
   -v "$(pwd)/.${DOCKER_ENGINE}/.ivy2/cache:/home/user/.ivy2/cache" \
   --workdir "$(pwd)" \
-  spinal-image "$@"
+  $IMAGE_NAME "$@"
