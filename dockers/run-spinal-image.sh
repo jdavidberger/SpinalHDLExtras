48332a586b41bd0d5cb95694fba1922e24882757
Justin Berger <j.david.berger@gmail.com>
2025-12-09 20:27:43 -0700

    Use userns host

--- dockers/run-spinal-image.sh
+++ dockers/run-spinal-image.sh
@@ -8,17 +8,19 @@ IMAGE_NAME=ghcr.io/jdavidberger/spinalhdlextras/spinal-image:latest
 if [[ ! -z $PULL_IMAGE ]]; then
   ${DOCKER_ENGINE} pull $IMAGE_NAME
 else
-  ${DOCKER_ENGINE} build --build-arg USER_UID=$(id -u ${USER}) $SCRIPT_DIR -f Dockerfile.spinalhdl -t $IMAGE_NAME --progress plain
+  ${DOCKER_ENGINE} build $SCRIPT_DIR -f Dockerfile.spinalhdl -t $IMAGE_NAME --progress plain
 fi
 
 mkdir -p "$(pwd)/.${DOCKER_ENGINE}/.cache"
 mkdir -p "$(pwd)/.${DOCKER_ENGINE}/.sbt"
 mkdir -p "$(pwd)/.${DOCKER_ENGINE}/.ivy2/cache"
+mkdir -p "$(pwd)/.${DOCKER_ENGINE}/target"
 
-${DOCKER_ENGINE} run --userns=keep-id \
+${DOCKER_ENGINE} run --userns=host \
   -it --rm \
   -v "$(pwd)/.${DOCKER_ENGINE}/.cache:/home/user/.cache" \
   -v "$(pwd):$(pwd)" \
+  -v "$(pwd)/.${DOCKER_ENGINE}/target:$(pwd)/target" \
   -v "$(pwd)/.${DOCKER_ENGINE}/.sbt:/home/user/.sbt" \
   -v "$(pwd)/.${DOCKER_ENGINE}/.ivy2/cache:/home/user/.ivy2/cache" \
   --workdir "$(pwd)" \
