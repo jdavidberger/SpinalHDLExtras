cdd209deab2474dd95db17f46a4310f587f1ba83
Justin Berger <j.david.berger@gmail.com>
2026-06-29 10:31:31 -0600

    CI spin

--- dockers/run-spinal-image.sh
+++ dockers/run-spinal-image.sh
@@ -12,17 +12,25 @@ else
   ${DOCKER_ENGINE} build $SCRIPT_DIR -f Dockerfile.spinalhdl -t $IMAGE_NAME --progress plain
 fi
 
-mkdir -p "$(pwd)/.${DOCKER_ENGINE}/.cache"
-mkdir -p "$(pwd)/.${DOCKER_ENGINE}/.sbt"
-mkdir -p "$(pwd)/.${DOCKER_ENGINE}/.ivy2/cache"
-mkdir -p "$(pwd)/.${DOCKER_ENGINE}/target"
+PWD=$(pwd)
+
+mkdir -p "${PWD}/.${DOCKER_ENGINE}/.cache"
+mkdir -p "${PWD}/.${DOCKER_ENGINE}/.sbt"
+mkdir -p "${PWD}/.${DOCKER_ENGINE}/.ivy2/cache"
+mkdir -p "${PWD}/.${DOCKER_ENGINE}/target"
+mkdir -p "${PWD}/.${DOCKER_ENGINE}/simulations"
+mkdir -p "${PWD}/.${DOCKER_ENGINE}/simWorkspace"
+mkdir -p "${PWD}/.${DOCKER_ENGINE}/project"
 
 ${DOCKER_ENGINE} run --userns=host \
   -it --rm \
-  -v "$(pwd)/.${DOCKER_ENGINE}/.cache:/home/user/.cache" \
-  -v "$(pwd):$(pwd)" \
-  -v "$(pwd)/.${DOCKER_ENGINE}/target:$(pwd)/target" \
-  -v "$(pwd)/.${DOCKER_ENGINE}/.sbt:/home/user/.sbt" \
-  -v "$(pwd)/.${DOCKER_ENGINE}/.ivy2/cache:/home/user/.ivy2/cache" \
-  --workdir "$(pwd)" \
+  -v "${PWD}/.${DOCKER_ENGINE}/.cache:/home/user/.cache" \
+  -v "${PWD}:${PWD}" \
+  -v "${PWD}/.${DOCKER_ENGINE}/project:${PWD}/project" \
+  -v "${PWD}/.${DOCKER_ENGINE}/target:${PWD}/target" \
+  -v "${PWD}/.${DOCKER_ENGINE}/.sbt:/home/user/.sbt" \
+  -v "${PWD}/.${DOCKER_ENGINE}/simulations:${PWD}/simulations" \
+  -v "${PWD}/.${DOCKER_ENGINE}/simWorkspace:${PWD}/simWorkspace" \
+  -v "${PWD}/.${DOCKER_ENGINE}/.ivy2/cache:/home/user/.ivy2/cache" \
+  --workdir "${PWD}" \
   $IMAGE_NAME "$@"
